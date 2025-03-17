edd9815609398464ecdc7dc146a1064ad0e06c77
Rostyslav <159663154+Rostyslav-Shevchuk@users.noreply.github.com>
2025-03-17 22:15:21 +0200

    Initial Commit

---
+++ MEALO/MEALOApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEALOApp.swift
+//  MEALO
+//
+//  Created by Ростислав on 17.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MEALOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
